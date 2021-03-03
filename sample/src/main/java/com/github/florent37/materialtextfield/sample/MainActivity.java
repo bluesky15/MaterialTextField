84ffb5150427747317a88daf41c368faf98f505f
Lalit Behera <lalit143.er@gmail.com>
2021-03-03 20:26:17 +0530

    gradle and fabric gradle class path updated project migrated to androidx

--- sample/src/main/java/com/github/florent37/materialtextfield/sample/MainActivity.java
+++ sample/src/main/java/com/github/florent37/materialtextfield/sample/MainActivity.java
@@ -2,9 +2,11 @@ package com.github.florent37.materialtextfield.sample;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.crashlytics.android.Crashlytics;
 
